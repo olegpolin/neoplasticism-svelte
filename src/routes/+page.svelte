67ace518fe42a5671774c747ed6d26c5b4dea1ea
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-24 10:35:53 -0400

    Update title in hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import HeroSection from '$lib/components/hero-section.svelte';
   import RootComponents from '$lib/components/root-components.svelte';
 
-  const title = 'The Foundation for your Design System';
+  const title = 'Neoplasticism Design System';
   const description =
     'A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.';
 </script>
